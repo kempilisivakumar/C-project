89bedaf0e214284b9be4f87ca9b6f8fa6297d80f
Kempili Sivakumar <kempilisiva.ind@gmail.com>
2025-07-14 10:06:37 +0000

    added rvr.c and makfile and modified main.c

--- main.c
+++ main.c
@@ -1,14 +1,17 @@
 //#include <stdio.h>
 
 // Function declarations (prototypes)
+
 void biggest3();
 void factorial();
 void palindrome();
+void reverse();
 
 int main() {
     biggest3();
     factorial();
     palindrome();
+    reverse();
 //    return 0;
 }
 
